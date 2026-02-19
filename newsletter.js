930cbc4ae080dfa4e7be53142b3f60de2c45b979
Aleksandar Orbanic <aleksandar.orbanic@student.uniri.hr>
2026-02-19 18:17:17 +0100

    promijena

--- newsletter.js
+++ newsletter.js
@@ -3,7 +3,7 @@ const form = document.getElementById("newsletter-form");
 
 form.addEventListener("submit", function(e){
   e.preventDefault();
-  text.textContent = "Hvala što ste se učlanili u naš newsletter";
+  text.textContent = "Hvala što ste se prijavili na naš newsletter";
   form.remove();
 });
 
